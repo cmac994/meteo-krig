56951be5baa82193d06d4c16a009c77b9401b567
Callie McNicholas <cmcnicholas94@gmail.com>
2021-03-10 22:21:11 -0800

    Update

--- notebooks/funcs.py
+++ notebooks/funcs.py
@@ -42,3 +42,37 @@ def date_list(start_date,end_date,step,format_str):
 		d00 += timedelta(0,step)
 
 	return dt_list
+
+R = 6371.
+def haversine(lat1, lng1, lat2, lng2):
+        """
+        Use Haversine formula to estimate distances from all
+        gridpoints to a given location (lat, lng)
+        """
+
+        lat1 = np.radians(lat1); lat2 = np.radians(lat2)
+        lng1 = np.radians(lng1); lng2 = np.radians(lng2)
+
+        R = 6371. # Radius of earth in km
+        dlat = lat2 - lat1
+        dlng = lng2 - lng1
+        a = np.sin(dlat/2)**2 + np.cos(lat2) * np.cos(lat1) * \
+                np.sin(dlng/2)**2
+        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1.0-a))
+        return R*c
+
+def get_pos(brng,d,lon1,lat1):
+        R = 6371.
+        lat1 = np.radians(lat1) #Current lat point converted to radians
+        lon1 = np.radians(lon1) #Current long point converted to radians
+
+        lat2 = np.arcsin( np.sin(lat1)*np.cos(d/R) +
+         np.cos(lat1)*np.sin(d/R)*np.cos(brng))
+
+        lon2 = lon1 + np.arctan2(np.sin(brng)*np.sin(d/R)*np.cos(lat1),
+                 np.cos(d/R)-np.sin(lat1)*np.sin(lat2))
+
+        lat2 = np.degrees(lat2)
+        lon2 = np.degrees(lon2)
+
+        return lon2,lat2
